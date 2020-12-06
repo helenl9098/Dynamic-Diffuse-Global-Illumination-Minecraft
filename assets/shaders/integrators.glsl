40213afc45bbcfee75ac0cfadb2db456b3f98f01
Janine Liu <j9liu@users.noreply.github.com>
2020-12-06 01:46:09 -0500

    Commit before pull; added random hemisphere direction code

--- assets/shaders/integrators.glsl
+++ assets/shaders/integrators.glsl
@@ -73,7 +73,8 @@ vec3 integrator_binary
     }
 
     // CHANGED: direct lighting
-    Ray light_feeler = Ray(info.pos, normalize(get_light_pos_in_scene(render_settings.scene) - info.pos)); // this is just a hack so the light feeler ray can be caluclated by the get intersection
+    // this is just a hack so the light feeler ray can be calculated by the get intersection
+    Ray light_feeler = Ray(info.pos, normalize(get_light_pos_in_scene(render_settings.scene) - info.pos));
     if (intersect_scene(light_feeler, mint, maxt, temp_info)) {
 
         if (temp_info.type == 2) {
